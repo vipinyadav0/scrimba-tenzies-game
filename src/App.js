2656e228cdb4cdc15d343dda1df3371bbd3a0537
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-12-17 23:18:31 +0530

    Dice held and remaining rolled

--- src/App.js
+++ src/App.js
@@ -23,11 +23,16 @@ function App() {
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return !die.isHeld ? { ...die, value: Math.ceil(Math.random() * 6) } : die
+    }))
   }
 
   function holdDice(id) {
-    console.log(id)
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } : die
+    }))
   }
 
   const diceElements = dice.map(die => (
@@ -36,6 +41,8 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
