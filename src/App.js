332ead910eff0d60d4fcae5819c74231ddc0af9a
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-12-17 14:20:39 +0530

    Die component rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
+import Die from './components/Die';
+
 
 function App() {
+
+
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Die value={die} />)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
+    </main>
+  )
 }
 
 export default App;
