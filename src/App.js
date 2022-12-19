cbaad71a32f130b3f5b262a580ce715e6bf2b282
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-12-19 20:24:23 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { nanoid } from "nanoid"
 import Confetti from 'react-confetti'
 
 
+
 function App() {
-
-
   const [dice, setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
+  const [count, setCount] = useState(0)
 
 
   React.useEffect(() => {
@@ -36,6 +36,7 @@ function App() {
   }
 
   function rollDice() {
+    setCount(count + 1)
     if (!tenzies) {
       setDice(oldDice => oldDice.map(die => {
         return !die.isHeld ? { ...die, value: Math.ceil(Math.random() * 6) } : die
@@ -45,6 +46,7 @@ function App() {
       setDice(allNewDice)
     }
   }
+  console.log(count)
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
@@ -59,12 +61,15 @@ function App() {
 
   return (
     <main>
-      {tenzies ? <Confetti /> : ""}
+      <div className='confetti'>
+        {tenzies ? <Confetti /> : ""}
+      </div>
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
+      <h1 className='won'>  {!tenzies ? " " : `Congrats! You won in ${count} click.`}</h1>
       <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
